aabe350b7ea32ceaac4cad41edc8bc2686a93e0a
Rhet Turnbull <rturnbull@gmail.com>
2023-12-08 21:15:33 -0800

    Added tests for specific CLI frameworks

--- tests/test_pydantic_argparse.py
+++ tests/test_pydantic_argparse.py
@@ -1,49 +1,45 @@
 """Test clirunner with pydantic-argparse"""
 
-import pytest
-
-try:
-    import pydantic
-    import pydantic_argparse
-except ImportError:
-    pytest.skip("pydantic-argparse not installed", allow_module_level=True)
+import instld
 
 from clirunner import CliRunner
 
 
-class Arguments(pydantic.BaseModel):
-    # Required Args
-    string: str = pydantic.Field(description="a required string")
-    integer: int = pydantic.Field(description="a required integer")
-    flag: bool = pydantic.Field(description="a required flag")
-
-    # Optional Args
-    second_flag: bool = pydantic.Field(False, description="an optional flag")
-    third_flag: bool = pydantic.Field(True, description="an optional flag")
-
-
-def main() -> None:
-    # Create Parser and Parse Args
-    parser = pydantic_argparse.ArgumentParser(
-        model=Arguments,
-        prog="Example Program",
-        description="Example Description",
-        version="0.0.1",
-        epilog="Example Epilog",
-    )
-    args = parser.parse_typed_args()
-
-    # Print Args
-    print(args)
-
-
 def test_pydantic_argparse():
-    runner = CliRunner()
-    result = runner.invoke(
-        main, ["--string", "foo", "--integer", "42", "--flag", "--no-third-flag"]
-    )
-    assert result.exit_code == 0
-    assert (
-        result.output
-        == "string='foo' integer=42 flag=True second_flag=False third_flag=False\n"
-    )
+    with instld("pydantic-argparse==0.8.0", "pydantic==1.9.0") as context:
+        pydantic = context.import_here("pydantic")
+        pydantic_argparse = context.import_here("pydantic_argparse")
+
+        class Arguments(pydantic.BaseModel):  # type: ignore
+            # Required Args
+            string: str = pydantic.Field(description="a required string")  # type: ignore
+            integer: int = pydantic.Field(description="a required integer")  # type: ignore
+            flag: bool = pydantic.Field(description="a required flag")  # type: ignore
+
+            # Optional Args
+            second_flag: bool = pydantic.Field(False, description="an optional flag")  # type: ignore
+            third_flag: bool = pydantic.Field(True, description="an optional flag")  # type: ignore
+
+        def main() -> None:
+            # Create Parser and Parse Args
+            parser = pydantic_argparse.ArgumentParser(  # type: ignore
+                model=Arguments,
+                prog="Example Program",
+                description="Example Description",
+                version="0.0.1",
+                epilog="Example Epilog",
+            )
+            args = parser.parse_typed_args()
+
+            # Print Args
+            print(args)
+
+        runner = CliRunner()
+        result = runner.invoke(
+            main, ["--string", "foo", "--integer", "42", "--flag", "--no-third-flag"]
+        )
+        assert result.exit_code == 0
+        assert (
+            result.output
+            == "string='foo' integer=42 flag=True second_flag=False third_flag=False\n"
+        )
--- tests/test_tyro.py
+++ tests/test_tyro.py
@@ -2,53 +2,52 @@
 
 from dataclasses import dataclass
 
-import pytest
+import instld
 
 from clirunner import CliRunner
 
-try:
-    import tyro
-except ImportError:
-    pytest.skip("tyro not installed", allow_module_level=True)
-
-
-def cli_function():
-    def add(a: int, b: int = 3) -> int:
-        return a + b
-
-    # Populate the inputs of add(), call it, then return the output.
-    total = tyro.cli(add)
-
-    print(total)
-
-
-def cli_dataclass():
-    @dataclass
-    class Args:
-        a: int
-        b: int = 3
-
-    args = tyro.cli(Args)
-    print(args.a + args.b)
-
 
 def test_cli_function():
-    runner = CliRunner()
-    result = runner.invoke(cli_function, ["--a", "1", "--b", "2"])
-    assert not result.exception
-    assert result.output == "3\n"
+    with instld("tyro") as context:
+        tyro = context.import_here("tyro")
 
-    result = runner.invoke(cli_function, ["--a", "1"])
-    assert not result.exception
-    assert result.output == "4\n"
+        def cli_function():
+            def add(a: int, b: int = 3) -> int:
+                return a + b
+
+            # Populate the inputs of add(), call it, then return the output.
+            total = tyro.cli(add)
+
+            print(total)
+
+        runner = CliRunner()
+        result = runner.invoke(cli_function, ["--a", "1", "--b", "2"])
+        assert not result.exception
+        assert result.output == "3\n"
+
+        result = runner.invoke(cli_function, ["--a", "1"])
+        assert not result.exception
+        assert result.output == "4\n"
 
 
 def test_cli_dataclass():
-    runner = CliRunner()
-    result = runner.invoke(cli_dataclass, ["--a", "1", "--b", "2"])
-    assert not result.exception
-    assert result.output == "3\n"
+    with instld("tyro==0.6.0") as context:
+        tyro = context.import_here("tyro")
 
-    result = runner.invoke(cli_dataclass, ["--a", "1"])
-    assert not result.exception
-    assert result.output == "4\n"
+        def cli_dataclass():
+            @dataclass
+            class Args:
+                a: int
+                b: int = 3
+
+            args = tyro.cli(Args)
+            print(args.a + args.b)
+
+        runner = CliRunner()
+        result = runner.invoke(cli_dataclass, ["--a", "1", "--b", "2"])
+        assert not result.exception
+        assert result.output == "3\n"
+
+        result = runner.invoke(cli_dataclass, ["--a", "1"])
+        assert not result.exception
+        assert result.output == "4\n"
